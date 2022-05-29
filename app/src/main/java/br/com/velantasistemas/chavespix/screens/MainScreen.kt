63cc3adc6395c8e9dc43fd9729950fee7abe2ed3
pedrofsn <pedrokra@gmail.com>
2022-05-28 23:45:11 -0300

    Main screen

---
+++ app/src/main/java/br/com/velantasistemas/chavespix/screens/MainScreen.kt
@@ -0,0 +1,62 @@
+package br.com.velantasistemas.chavespix.screens
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import br.com.velantasistemas.chavespix.BottomNavGraph
+
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = { BottomBar(navHostController = navController) }
+    ) {
+        BottomNavGraph(navHostController = navController)
+    }
+}
+
+@Composable
+fun BottomBar(navHostController: NavHostController) {
+    val screens = listOf(
+        BottomBarMenu.Home,
+        BottomBarMenu.Profile,
+        BottomBarMenu.Settings,
+    )
+    val navBackStackEntry by navHostController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+
+    BottomNavigation {
+        screens.forEach { screen ->
+            AddItem(
+                screen = screen,
+                currentDestination = currentDestination,
+                navHostController = navHostController
+            )
+        }
+    }
+
+}
+
+@Composable
+fun RowScope.AddItem(
+    screen: BottomBarMenu,
+    currentDestination: NavDestination?,
+    navHostController: NavHostController
+) {
+    BottomNavigationItem(
+        label = { Text(text = screen.title) },
+        icon = { Icon(imageVector = screen.icon, contentDescription = "Navigation Icon") },
+        selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+        onClick = { navHostController.navigate(screen.route) }
+    )
+}
